b93a40f931fb8d666339ca4c263b7d10f0b913ba
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-04-02 11:47:08 -0300

    Starter point done in the repository with git file, README.md and font.js

---
+++ font.js
@@ -0,0 +1,15 @@
+/*  
+    This is the first lesson.. 
+    This 'font.js' was created typing in terminal:
+    code font.js
+    --> After created i moved to the main folder /CompleteWeb
+*/
+
+console.log('Test');
+
+
+/* 
+    I'he downloaded the extension 'Code Runner 0.9.7' in my VSCode...
+    These extension RUNS Js codes locally... using Ctrl+Alt+N you
+    can make Node do this job 4u ;)
+*/

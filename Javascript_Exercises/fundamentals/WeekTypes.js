d5cbad36f0aad62d97f523b97d7af29c0321b09f
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-04-03 23:41:22 -0300

    WeekTypes.js is so explict to explain what this code shows

---
+++ Javascript_Exercises/fundamentals/WeekTypes.js
@@ -0,0 +1,9 @@
+/* Once weekly typed, JS has types  */
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+/* Changing types dinamically */
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)

f99d201480d4e73c5cae6382107c695a44195ca1
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-04-04 01:38:12 -0300

    numbersWatchOut.js has some behavior of JS to eork with Numbers

--- Javascript_Exercises/fundamentals/numbers.js
+++ Javascript_Exercises/fundamentals/numbers.js
@@ -4,7 +4,6 @@ const peso2 = Number('2.0')
 console.log(peso1, peso2)
 
 // Both of variables are INT
-console.log(peso1, peso2)
 console.log(Number.isInteger(peso1))
 console.log(Number.isInteger(peso2))
 
---
+++ Javascript_Exercises/fundamentals/numbersWatchOut.js
@@ -0,0 +1,18 @@
+//Infinity
+console.log(7 / 0) 
+
+//Natural convertion of String to Integer
+console.log("10" / 2) 
+
+//NaN
+console.log("Show!" * 2)
+
+//Showing the error of precision in JS
+console.log(0.1 + 0.7)
+
+/* Cannot convert native type to String, put parenthesis for security  */
+//console.log(10.toString())
+console.log((10).toString())
+
+// In parenthesis the function runs well
+console.log((10.345).toFixed(2))

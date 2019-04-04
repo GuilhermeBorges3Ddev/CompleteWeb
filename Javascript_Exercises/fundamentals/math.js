2a66eecd7ccca8cab77a9d35527a76e42ed5d1bb
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-04-04 02:01:31 -0300

    Math.js uses the object Math to show one of his const's and one of his functions

---
+++ Javascript_Exercises/fundamentals/math.js
@@ -0,0 +1,6 @@
+/* Using the Object Math, in JS it's not a function */
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)//PI is a const of the Object 'Math'
+
+console.log(area)
+console.log(typeof Math)

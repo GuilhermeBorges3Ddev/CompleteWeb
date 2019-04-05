3162b7d2d06495885c350a5891363490b14f63c0
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-04-05 03:39:19 -0300

    booleans.js has examples that JS booleans are more than true or false, or, 0 or 1

---
+++ Javascript_Exercises/fundamentals/booleans.js
@@ -0,0 +1,41 @@
+/* Returning two trues in different ways */
+let isActive = false
+console.log(isActive)
+isAdtive = true
+console.log(isActive)
+
+/* Double negative is the same of True  */
+isActive = 1
+console.log(!isActive)
+console.log(!!isActive)
+
+/* Numbers, Strings, Arrays, Objects, Infinity and atribuitions are TRUE */
+console.log('Thinks that returns true...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!'texto')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isActive = true))
+
+/* '0', empty strings, null, NaN and undefined are FALSE */
+console.log('Thinks that returns false...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isActive = false))
+
+/* Logical sentence what return TRUE ngativated two times is true */
+console.log('if one value is true into a multiple "OR" the sentence is True')
+console.log(!!('' || null || 0 || 'opa' || 123))
+
+/* */
+let name = 'Guilherme'
+console.log(name || 'Without name')
+
+
+

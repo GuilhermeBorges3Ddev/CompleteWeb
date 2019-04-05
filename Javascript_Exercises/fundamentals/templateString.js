861f7ead3578c621c2f2227b8fe61b72bc874fe2
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-04-05 02:05:18 -0300

    templateString.js has some examples of these string declaration applyed in a function and with a variable passed into a string

---
+++ Javascript_Exercises/fundamentals/templateString.js
@@ -0,0 +1,17 @@
+//Doing the 'Concat()' operation without template string
+const nome = 'Rebeca'
+const concatenacao = 'Olá' + nome + '!'
+
+//Concatenation with template string
+const template = `Olá ${nome} !`
+
+//We'll see the same result, but done in different ways
+console.log(concatenacao + '---->' + template)
+
+
+//Expressions...
+console.log(`1 + 1 = ${1 + 1}`)
+
+//Combining arrow functions with Strings
+const up = text => texto.toUppercase()
+console.log(`Ei... ${up('cuidado')}!`)

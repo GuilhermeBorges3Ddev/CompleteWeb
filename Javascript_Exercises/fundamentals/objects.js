b8073a19717d35c83e6a73e1babc47b8afbf2c2d
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-04-07 00:25:22 -0300

    objects.js has two ways to declare an object in JS

---
+++ Javascript_Exercises/fundamentals/objects.js
@@ -0,0 +1,22 @@
+/* Adding attributes inside the object by key and th respective value */
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40//Its not the perfect form to declare a pair key/value
+console.log(prod1)
+console.log('-----------------------------')
+
+/* Using and declaring an object in the 'open-format' */
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    obj: {
+        categoria: 'Vestuario',
+        obj:{
+            subcategoria: 'Camisas Masculinas'
+        }
+    }
+}
+console.log(prod2)
+console.log('-----------------------------')
+

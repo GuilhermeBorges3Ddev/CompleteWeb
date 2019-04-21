9d3257d65d5782f2f8a953f65b672d605cdf35ca
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-04-20 22:34:47 -0300

    assignment.js explains assignment combined with some operators

---
+++ Javascript_Exercises/fundamentals/assignment.js
@@ -0,0 +1,29 @@
+//Direct assignment
+const a = 7 
+let b = 3
+
+//Additive assignment
+b += a
+console.log(b)
+console.log('---------------')
+
+//Subtraction assignment
+b -= 4
+console.log(b)
+console.log('---------------')
+
+//Multiplicative assignment
+b *= 2
+console.log(b)
+console.log('---------------')
+
+//Divide and assignment
+b /= 2
+console.log(b)
+console.log('---------------')
+
+//Module assignment
+b %= 2
+console.log(b)
+console.log('---------------')
+

87439480df80d4a2703c718eaa9b96236adc0f03
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-05-18 02:56:02 -0300

    aritmeticOperators.js has only binary operations to build into consts and definitions of INLINE, PREFIX andPOSFIX over the comments

---
+++ Javascript_Exercises/fundamentals/aritmeticOperators.js
@@ -0,0 +1,15 @@
+/*
+    Aritmetic operators works only in "binary operations/inline declaration":
+    INLINE :: const x = y + z
+    PREFIX :: ++x
+    POSFIX :: x--
+*/
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
